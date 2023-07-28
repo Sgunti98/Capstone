d0ca885ddfade12a566bda32e490204d89189235
Sgunti98 <131033894+Sgunti98@users.noreply.github.com>
2023-07-28 10:48:22 -0400

    Add files via upload

---
+++ G_Sravanthi_EmployeeMgmtSystem/G_Sravanthi_EmployeeMgmtSystem/src/main/java/com/example/G_Sravanthi_EmployeeMgmtSystem/GSravanthiEmployeeMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.G_Sravanthi_EmployeeMgmtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GSravanthiEmployeeMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GSravanthiEmployeeMgmtSystemApplication.class, args);
+	}
+
+}
